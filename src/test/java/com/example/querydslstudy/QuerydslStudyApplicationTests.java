ea06e56fb3501759baf226404124856e944f06ba
fistkim101 <fistkim101@gmail.com>
2023-03-30 11:23:55 +0900

    build : 프로젝트 환경 구성

--- src/test/java/com/example/querydslstudy/QuerydslStudyApplicationTests.java
+++ src/test/java/com/example/querydslstudy/QuerydslStudyApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.querydslstudy;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class QuerydslStudyApplicationTests {
 
+    @Autowired
+    EntityManager em;
+
     @Test
-    void contextLoads() {
+    @Rollback(value = false)
+    void sampleTest() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        QHello qHello = QHello.hello;
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertThat(result).isEqualTo(hello);
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
